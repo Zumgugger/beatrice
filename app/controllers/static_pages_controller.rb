871c363ac450bdf2530c7098cd9c781bd6442c9c
zumgugger <markus.gugger@gmail.com>
2018-07-11 12:58:27 +0200

    google maps added

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -17,5 +17,9 @@ before_action :store_location
       @snippets=Snippet.all
   end
     
+    def google_map(center)
+  "https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=300x300&zoom=17"
+    end
+    
 end
 
